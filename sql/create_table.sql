e1f634603a19de933ea3ef9a5068ca410f3e5fcf
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2024-03-22 23:46:31 -0300

    feat: realizar criacao de tabela do mysql de maneira automatizada ao subir o docker

---
+++ sql/create_table.sql
@@ -0,0 +1,10 @@
+USE `orders`;
+
+CREATE TABLE IF NOT EXISTS orders (
+    `id` VARCHAR(255) NOT NULL,
+    `price` FLOAT NOT NULL,
+    `tax` FLOAT NOT NULL,
+    `final_price` FLOAT NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
